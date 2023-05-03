b5f6fed3c826035469de8f928a3e85dc2093c0fd
Christian Moronta <cmoronta26@gmail.com>
2023-05-03 02:36:54 -0400

    Add Lato font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
@@ -6,22 +7,24 @@ import Projects from "./components/Projects";
 import Who from "./components/Who";
 import Container from "./components/Container";
 import NavBar from "./components/NavBar";
-import "./index.css";
 import SectionContainer from "./components/SectionContainer";
+import "./index.css";
 
 function App() {
-  return (
-    <Container>
-      <SectionContainer>
-        <Hero />
-        <Who />
-        <Experience />
-        <Projects />
-        <Contact />
-      </SectionContainer>
-      <NavBar />
-    </Container>
-  );
+	return (
+		<ChakraProvider theme={theme}>
+			<Container>
+				<SectionContainer>
+					<Hero />
+					<Who />
+					<Experience />
+					<Projects />
+					<Contact />
+				</SectionContainer>
+				<NavBar />
+			</Container>
+		</ChakraProvider>
+	);
 }
 
 export default App;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,16 @@ import Section from "./Section";
 export interface HeroProps extends SectionProps {}
 
 export default function Hero(props: HeroProps) {
-  return (
-    <Section>
-      <Flex direction="column" height="100%">
-        <Image borderRadius="full" boxSize="150px" src="public/photo.jpg" />
-        <Text>CHRISTIAN MORONTA</Text>
-        <Text>
-          BLAH BLAH BLAH INTEREST IN REACT, TYPESCRIPT, DIGITIAL SIGNALS
-          PROCESSING, and more dumbass shit
-        </Text>
-      </Flex>
-    </Section>
-  );
+	return (
+		<Section>
+			<Flex direction="column" height="100%">
+				<Image borderRadius="full" boxSize="150px" src="photo.jpg" />
+				<Text>CHRISTIAN MORONTA</Text>
+				<Text>
+					BLAH BLAH BLAH INTEREST IN REACT, TYPESCRIPT, DIGITIAL SIGNALS
+					PROCESSING, and more dumbass shit
+				</Text>
+			</Flex>
+		</Section>
+	);
 }
