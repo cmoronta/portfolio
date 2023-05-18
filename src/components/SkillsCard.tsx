5e427212d65c4a5cf592360116950f612b0ad879
Christian Moronta <cmoronta26@gmail.com>
2023-05-18 02:55:55 -0400

    Add 3d models to skill cards

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -1,34 +1,70 @@
-import { GridItem, Flex, Text, Box, HStack, Heading } from "@chakra-ui/react";
+import { Flex, Text, Box, Heading } from "@chakra-ui/react";
 import { BoxProps } from "@chakra-ui/react";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { OrbitControls, Center } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
 
 export interface SkillCardsProps extends BoxProps {
   title: string;
   content: string;
+  model: string;
+  scale: number;
 }
+
+type SceneProps = {
+  model: string;
+  scale: number;
+};
+
+const Scene = ({ model, scale }: SceneProps) => {
+  const obj = useLoader(GLTFLoader, model);
+  return <primitive object={obj.scene} scale={scale} />;
+};
 
 export default function SkillsCard(props: SkillCardsProps) {
   return (
     <>
-      <Box
+      <Flex
         p={6}
         bg="rgba( 255, 255, 255, 0.05 )"
         borderRadius="20px"
         backdropBlur="8px"
         backdropFilter="blur(3px)"
         boxShadow="lg"
-        w="100%"
+        height={["auto", null, null, null, "12rem"]}
       >
-        <Flex justifyContent="center" alignItems="center">
-          <Flex flexGrow="10">
-            <Box width="100%" ml={2}>
-              <Heading>{props.title}</Heading>
-              <Text fontSize="xl" mt={4}>
-                {props.content}
-              </Text>
-            </Box>
-          </Flex>
+        <Flex
+          justifyItems="space-between"
+          alignItems={{ xl: "center" }}
+          width="100%"
+          direction={{
+            base: "column",
+            xl: "row",
+          }}
+        >
+          {/** React Three Fiber Canvas */}
+          <Box width={{ base: "80%", xl: "15%" }} alignSelf="center">
+            <Canvas>
+              <Suspense fallback={null}>
+                <Center rotation={[0.4, -0.7, 0]}>
+                  <ambientLight />
+                  <Scene scale={props.scale} model={props.model} />
+                  <OrbitControls />
+                </Center>
+              </Suspense>
+            </Canvas>
+          </Box>
+          {/** Heading and Subtext */}
+          <Box width={{ base: "100%", xl: "50%" }} ml={{ lg: 2 }}>
+            <Heading>{props.title}</Heading>
+            <Text fontSize="xl" mt={2}>
+              {props.content}
+            </Text>
+          </Box>
         </Flex>
-      </Box>
+      </Flex>
     </>
   );
 }
--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -11,30 +11,46 @@ export default function Skills(props: SkillsProps) {
       title: "Tools & Tech",
       content:
         "React / Chakra-UI / Next / Node / Linux / WSL2.0 / MongoDB / SQL / SQLite / Ansible / Jira / Git / GitHub / BitBucket",
+      model: "/models/Wrench.glb",
+      scale: 20,
     },
     {
       title: "Cloud",
       content: "AWS S3 / AWS EC2 / AWS VPC / Azure Virtual Machines",
+      model: "/models/Cloud.glb",
+      scale: 0.01,
     },
     {
       title: "Languages",
       content:
         "Bash / Python / JavaScript / Java / PowerShell / HTML / CSS / JSX / C / TypeScript",
+      model: "/models/MacBook Pro.glb",
+      scale: 1.7,
     },
     {
       title: "Personal",
       content:
         "People Skills / Communication Skills / Collaborative /  Spanish Speaker / Drums / Bass / Guitar / Piano",
+      model: "/models/Guitar Amp.glb",
+      scale: 0.01,
     },
   ];
   let cards = cardData.map((data: SkillCardsProps) => {
-    return <SkillsCard title={data.title} content={data.content} />;
+    return (
+      <SkillsCard
+        scale={data.scale}
+        model={data.model}
+        key={data.title}
+        title={data.title}
+        content={data.content}
+      />
+    );
   });
 
   return (
     <Section>
       <Heading fontSize="6xl">Skills</Heading>
-      <SimpleGrid padding={8} spacing={4} columns={[1, null, 2, null, 1]}>
+      <SimpleGrid p={2} spacing={4} columns={[1, null, 2, null, 1]}>
         {cards}
       </SimpleGrid>
     </Section>
