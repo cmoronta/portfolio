02ad472ba0b3f6efd2176b9ea28b01355f75d760
Christian Moronta <cmoronta26@gmail.com>
2023-06-07 16:34:19 -0400

    Adjust scroll snap so, only on for xl size screens

--- src/components/sections/Section.tsx
+++ src/components/sections/Section.tsx
@@ -1,21 +1,21 @@
 import { Box, BoxProps, Heading } from "@chakra-ui/react";
 
 export interface SectionProps extends BoxProps {
-  heading: null | string;
+	heading: null | string;
 }
 
 export default function Section(props: SectionProps) {
-  return (
-    <Box
-      scrollSnapAlign={{ base: "none", md: "start" }}
-      height={{ md: "100vh" }}
-      p={4}
-      width={{ base: "100%", md: "97%" }}
-    >
-      <Heading ml={8} fontSize={["2xl", "3xl", "4xl", "5xl", "6xl"]}>
-        {props.heading}
-      </Heading>
-      {props?.children}
-    </Box>
-  );
+	return (
+		<Box
+			scrollSnapAlign={{ base: "none", xl: "start" }}
+			height={{ xl: "100vh" }}
+			p={4}
+			width={{ base: "100%", md: "97%" }}
+		>
+			<Heading ml={8} fontSize={["2xl", "3xl", "4xl", "5xl", "6xl"]}>
+				{props.heading}
+			</Heading>
+			{props?.children}
+		</Box>
+	);
 }
