10f40d2a16afa1566b3551dff3ba5b4396e616d8
Christian Moronta <cmoronta26@gmail.com>
2023-06-23 03:07:40 -0400

    Fix padding on Projects and Experience sections

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -1,11 +1,11 @@
 import {
-	Box,
-	Flex,
-	HStack,
-	ListItem,
-	Stack,
-	Text,
-	UnorderedList,
+  Box,
+  Flex,
+  HStack,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
 } from "@chakra-ui/react";
 import { SectionProps } from "./Section";
 import Section from "./Section";
@@ -13,56 +13,58 @@ import Section from "./Section";
 export interface ExperienceProps extends SectionProps {}
 
 export default function Experience(props: ExperienceProps) {
-	let expStrings: string[] = [
-		"Developed Ansible and PowerShell scripts that improved the user experience for virtual desktop images",
-		"Used Git, Jira, and BitBucket to contribute code to a 15+ person team",
-		"Built out a full, cloud-based, security focused, ephemeral virtual-desktop-infrastructure (VDI) solution as part of updating documentation",
-		"Developed bash scripts to automate part of the VDI build out process",
-		"Contributed to infrastructure load testing for the VDI",
-	];
-	return (
-		<Section heading={props.heading}>
-			<Flex
-				mt={8}
-				bg="rgba( 255, 255, 255, 0.05 )"
-				borderRadius="20px"
-				backdropBlur="8px"
-				backdropFilter="blur(3px)"
-				boxShadow="lg"
-				height="85%"
-				alignItems="center"
-				direction="column"
-				p={8}
-			>
-				<Stack spacing={1} alignItems={{ base: "flex-start", lg: "center" }}>
-					<Text
-						fontWeight="bold"
-						fontSize={{ base: "2xl", md: "3xl", lg: "3xl", xl: "5xl" }}
-					>
-						Georgia Tech Research Institute
-					</Text>
-					<Text fontSize={{ base: "xl", md: "2xl", lg: "2xl", xl: "3xl" }}>
-						Cloud DevOps Engineer Intern (Remote)
-					</Text>
-					<Text fontSize={{ base: "lg", md: "xl", lg: "2xl", xl: "3xl" }}>
-						May 2020 - January 2021
-					</Text>
-				</Stack>
-				<Box w="100%" mt={8}>
-					<UnorderedList spacing={4}>
-						{expStrings.map((str, i) => {
-							return (
-								<ListItem
-									key={i}
-									fontSize={{ base: "md", md: "lg", lg: "2xl", xl: "3xl" }}
-								>
-									{str}
-								</ListItem>
-							);
-						})}
-					</UnorderedList>
-				</Box>
-			</Flex>
-		</Section>
-	);
+  let expStrings: string[] = [
+    "Developed Ansible and PowerShell scripts that improved the user experience for virtual desktop images",
+    "Used Git, Jira, and BitBucket to contribute code to a 15+ person team",
+    "Built out a full, cloud-based, security focused, ephemeral virtual-desktop-infrastructure (VDI) solution as part of updating documentation",
+    "Developed bash scripts to automate part of the VDI build out process",
+    "Contributed to infrastructure load testing for the VDI",
+  ];
+  return (
+    <Section heading={props.heading}>
+      <Flex
+        mt={8}
+        ml={8}
+        mr={8}
+        bg="rgba( 255, 255, 255, 0.05 )"
+        borderRadius="20px"
+        backdropBlur="8px"
+        backdropFilter="blur(3px)"
+        boxShadow="lg"
+        height="85%"
+        alignItems="center"
+        direction="column"
+        p={12}
+      >
+        <Stack spacing={1} alignItems={{ base: "flex-start", lg: "center" }}>
+          <Text
+            fontWeight="bold"
+            fontSize={{ base: "2xl", md: "3xl", lg: "3xl", xl: "5xl" }}
+          >
+            Georgia Tech Research Institute
+          </Text>
+          <Text fontSize={{ base: "xl", md: "2xl", lg: "2xl", xl: "3xl" }}>
+            Cloud DevOps Engineer Intern (Remote)
+          </Text>
+          <Text fontSize={{ base: "lg", md: "xl", lg: "2xl", xl: "3xl" }}>
+            May 2020 - January 2021
+          </Text>
+        </Stack>
+        <Box w="100%" mt={8}>
+          <UnorderedList spacing={4}>
+            {expStrings.map((str, i) => {
+              return (
+                <ListItem
+                  key={i}
+                  fontSize={{ base: "md", md: "lg", lg: "2xl", xl: "3xl" }}
+                >
+                  {str}
+                </ListItem>
+              );
+            })}
+          </UnorderedList>
+        </Box>
+      </Flex>
+    </Section>
+  );
 }
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -6,70 +6,70 @@ import ProjectCard from "../ProjectCard";
 export interface ProjectsProps extends SectionProps {}
 
 export default function Projects(props: ProjectsProps) {
-	let projects = [
-		{
-			title: "Wave",
-			text: "Open source Electron based desktop application used to help the process of transcribing music by ear for learning songs on aninstrument.",
-			tags: "React / Electron / ChakraUI / React-Router-Dom / Formik / YouTube Player API / WaveSurfer.js.",
-			date: "2023",
-			link: "https://github.com/cmoronta/wave-electron",
-		},
-		{
-			title: "ytmp3",
-			text: "CLI script that takes a YouTube video URL or ID as an argument and downloads it as an .mp3 to the specified folder. Option to convert to .aiff and/or to split the song by instrument using the demucs ML model",
-			tags: "Node / yargs / fluent-ffmpeg / ytdl-core",
-			date: "2022",
-			link: "https://github.com/cmoronta/ytmp3",
-		},
-		{
-			title: "Google Calendar Scripts",
-			text: "Used Python and Google's calendar API to generate calendar events from a CSV file to organize when social media content is to be posted for my band.",
-			tags: "Python / Google Calendar API",
-			date: "2023",
-			link: "https://github.com/cmoronta/scripts",
-		},
-		{
-			title: "Discord Bot",
-			text: 'Discord Bot that DMs a user when a new "free tickets to xyz" post on the Atlanta subreddit is posted.',
-			tags: "Python / Discord API / Reddit API",
-			date: "2023",
-			link: "https://github.com/cmoronta/ticket_bot",
-		},
-		{
-			title: "This website!",
-			text: "This is my portfolio site.",
-			tags: "React / TypeScript / ChakraUI / Framer Motion / React Three Fiber / MongoDB",
-			date: "2023",
-			link: "https://github.com/cmoronta/portfolio",
-		},
-	];
+  let projects = [
+    {
+      title: "Wave",
+      text: "Open source Electron based desktop application used to help the process of transcribing music by ear for learning songs on aninstrument.",
+      tags: "React / Electron / ChakraUI / React-Router-Dom / Formik / YouTube Player API / WaveSurfer.js.",
+      date: "2023",
+      link: "https://github.com/cmoronta/wave-electron",
+    },
+    {
+      title: "ytmp3",
+      text: "CLI script that takes a YouTube video URL or ID as an argument and downloads it as an .mp3 to the specified folder. Option to convert to .aiff and/or to split the song by instrument using the demucs ML model",
+      tags: "Node / yargs / fluent-ffmpeg / ytdl-core",
+      date: "2022",
+      link: "https://github.com/cmoronta/ytmp3",
+    },
+    {
+      title: "Google Calendar Scripts",
+      text: "Used Python and Google's calendar API to generate calendar events from a CSV file to organize when social media content is to be posted for my band.",
+      tags: "Python / Google Calendar API",
+      date: "2023",
+      link: "https://github.com/cmoronta/scripts",
+    },
+    {
+      title: "Discord Bot",
+      text: 'Discord Bot that DMs a user when a new "free tickets to xyz" post on the Atlanta subreddit is posted.',
+      tags: "Python / Discord API / Reddit API",
+      date: "2023",
+      link: "https://github.com/cmoronta/ticket_bot",
+    },
+    {
+      title: "This website!",
+      text: "This is my portfolio site.",
+      tags: "React / TypeScript / ChakraUI / Framer Motion / React Three Fiber / MongoDB",
+      date: "2023",
+      link: "https://github.com/cmoronta/portfolio",
+    },
+  ];
 
-	projects = projects.sort((a, b) => {
-		return parseInt(a.date) - parseInt(b.date);
-	});
+  projects = projects.sort((a, b) => {
+    return parseInt(a.date) - parseInt(b.date);
+  });
 
-	let projectCards = projects.map((project, i) => {
-		return (
-			<ProjectCard
-				title={project.title}
-				date={project.date}
-				text={project.text}
-				tags={project.tags}
-				link={project.link}
-				key={i}
-			/>
-		);
-	});
-	return (
-		<Section heading={props.heading}>
-			<SimpleGrid
-				height="100%"
-				p={4}
-				columns={{ base: 1, md: 2, lg: 3 }}
-				spacing={4}
-			>
-				{projectCards}
-			</SimpleGrid>
-		</Section>
-	);
+  let projectCards = projects.map((project, i) => {
+    return (
+      <ProjectCard
+        title={project.title}
+        date={project.date}
+        text={project.text}
+        tags={project.tags}
+        link={project.link}
+        key={i}
+      />
+    );
+  });
+  return (
+    <Section heading={props.heading}>
+      <SimpleGrid
+        height="100%"
+        p={8}
+        columns={{ base: 1, md: 2, lg: 3 }}
+        spacing={4}
+      >
+        {projectCards}
+      </SimpleGrid>
+    </Section>
+  );
 }
