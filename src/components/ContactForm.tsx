dfed99aa4f01bf13ae25b9b5ea97d583dd8f447c
Christian Moronta <cmoronta26@gmail.com>
2023-06-30 10:46:23 -0400

    Edit form flex and width

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   BoxProps,
   Button,
+  Flex,
   Heading,
   Input,
   Textarea,
@@ -24,54 +25,58 @@ export default function ContactForm(props: ContactFormProps) {
   });
   return (
     <form onSubmit={formik.handleSubmit}>
-      <VStack
-        height="100%"
-        justifyContent="center"
-        alignItems="flex-start"
-        spacing={4}
-        pl={{ xl: 16 }}
-        pr={{ xl: 16 }}
-      >
-        <Input
-          placeholder="Name"
-          variant="filled"
-          type="text"
-          name="name"
-          id="name"
-          p={2}
-          _focus={{ background: "white" }}
-          onChange={formik.handleChange}
-          value={formik.values.name}
-        />
-        <Input
-          _focus={{ background: "white" }}
-          p={2}
-          type="email"
-          name="email"
-          id="email"
-          variant="filled"
-          placeholder="Email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <Textarea
-          p={2}
-          variant="filled"
-          placeholder="Write Your Message"
-          height={64}
-          name="text"
-          id="text"
-          _focus={{
-            background: "white",
-          }}
-          resize="none"
-          onChange={formik.handleChange}
-          value={formik.values.text}
-        />
-        <Button type="submit" width="100%" bg="orange.300">
-          Submit
-        </Button>
-      </VStack>
+      <Flex h="100%" justifyContent="center">
+        <VStack
+          height="100%"
+          justifyContent="center"
+          alignItems="flex-start"
+          spacing={4}
+          width={{ base: "100%", lg: "80%" }}
+          pl={{ xl: 16 }}
+          pr={{ xl: 16 }}
+        >
+          <Heading size={{ base: "xl", lg: "2xl" }}>Send me an email!</Heading>
+          <Input
+            placeholder="Name"
+            variant="filled"
+            type="text"
+            name="name"
+            id="name"
+            p={2}
+            _focus={{ background: "white" }}
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          <Input
+            _focus={{ background: "white" }}
+            p={2}
+            type="email"
+            name="email"
+            id="email"
+            variant="filled"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <Textarea
+            p={2}
+            variant="filled"
+            placeholder="Write Your Message"
+            height={64}
+            name="text"
+            id="text"
+            _focus={{
+              background: "white",
+            }}
+            resize="none"
+            onChange={formik.handleChange}
+            value={formik.values.text}
+          />
+          <Button type="submit" width="100%" bg="orange.300">
+            Submit
+          </Button>
+        </VStack>
+      </Flex>
     </form>
   );
 }
